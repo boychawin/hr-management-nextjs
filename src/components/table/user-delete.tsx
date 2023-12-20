394d258f0541f4b32f5f7a239a0d3dbcc016dc87
boychawin <boychawin.com@gmail.com>
2023-12-20 16:35:21 +0700

    20 v1

--- src/components/table/user-delete.tsx
+++ src/components/table/user-delete.tsx
@@ -50,7 +50,7 @@ const FormDelete = ({ id, email }: Props) => {
           <label htmlFor="email" className="mb-3 text-center px-2 py-5 block text-base font-medium text-[#07074D]">
             Delete email: {email}
           </label>
-          <input hidden type="email" value={email} name="email" id="email" placeholder="example@domain.com" className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+          {/* <input hidden type="email" value={email} name="email" id="email" placeholder="example@domain.com" className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" /> */}
         </div>
         <div>
           <FormButton classNames="w-full rounded-xl border border-red-500 bg-red-500 px-4 py-1.5 text-sm font-medium text-white transition-all hover:bg-white hover:text-red-500 active:bg-stone-100 dark:border-stone-700 dark:hover:border-stone-200 dark:hover:bg-black dark:hover:text-white dark:active:bg-stone-800">Confirm</FormButton>
